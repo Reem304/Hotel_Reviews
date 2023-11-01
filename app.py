754a354c66f12f1ebae7350e424a1294dbe672a7
Reem304 <121408160+Reem304@users.noreply.github.com>
2023-11-01 19:51:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,26 +13,41 @@ st.title('Global Hotel Reviews')
 st.info('Just buiding a model')
 
 
-year = st.number_input('year', min_value=2011)
-
-Hotel_name_sel= st.selectbox('Hotel_name', [ 'barriere-le-majestic','InterContinental Presidente','Fairmont Monte Carlo','Warwick Geneva','Grand Fiesta Americana','Hyatt Regency Palais'])
-Hotel_name=hotels_dummies[Hotel_name_sel]
-
-City_sel=st.selectbox("City",['Cannes','Geneva','Monaco','Nice','Cancun'])
-City=City_dummies[City_sel]
+year = st.number_input('year', min_value=2012)
 
 Country_sel= st.selectbox('Country', ['Mexico',  'Switzerland', 'France'])
 Country=Country_dummies[Country_sel]
 
+if Country_sel == 'Mexico':
+    
+    City_sel=st.selectbox("City",['Cancun'])
+    
+    
+    Hotel_name_sel= st.selectbox('Hotel_name', ['InterContinental Presidente','Grand Fiesta Americana'])
+    
+    
+elif Country_sel == 'Switzerland':
+    
+    City_sel=st.selectbox("City",['Geneva'])
+    
+    Hotel_name_sel= st.selectbox('Hotel_name', ['Warwick Geneva'])
+    
+    
+elif Country_sel == 'France':
+    City_sel=st.selectbox("City",['Cannes','Monaco','Nice'])
+    
+    Hotel_name_sel= st.selectbox('Hotel_name', ['barriere-le-majestic','Fairmont Monte Carlo','Hyatt Regency Palais'])           
+    
+City=City_dummies[City_sel] 
+Hotel_name=hotels_dummies[Hotel_name_sel]
 
 
 data= year
 data=[]
 data.append(year)
-data.extend(Hotel_name)
-data.extend(City)
 data.extend(Country)
-
+data.extend(City)
+data.extend(Hotel_name)
 
 
 result = model.predict([data])
@@ -40,7 +55,3 @@ result = model.predict([data])
 
 st.write(result)
 st.success(f'Predicted Rating: {result[0]:.2f}')
-
-
-
-
